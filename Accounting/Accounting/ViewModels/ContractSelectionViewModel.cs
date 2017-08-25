b4df4222ea392c4e2e5dede9fe9c25f342caa745
mihaistancu <stancu_meehigh@yahoo.com>
2017-08-25 18:58:27 +0200

    Create dummy contracts and load the contract selection view in the main window

--- Accounting/Accounting/ViewModels/ContractSelectionViewModel.cs
+++ Accounting/Accounting/ViewModels/ContractSelectionViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Accounting.Models;
+using System;
 using System.Collections.ObjectModel;
 
 namespace Accounting.ViewModels
@@ -8,5 +9,36 @@ namespace Accounting.ViewModels
         public ObservableCollection<Contract> Contracts { get; set; }
 
         public Contract SelectedContract { get; set; }
+
+        public ContractSelectionViewModel()
+        {
+            Contracts = new ObservableCollection<Contract>(new []
+            {
+                new Contract
+                {
+                    Provider = new Provider
+                    {
+                        Name = "Provider1"
+                    },
+                    Buyer=new Buyer
+                    {
+                        Name = "Buyer1",
+                        DateOfBirth = new DateTime(1990,1,1)
+                    }
+                },
+                new Contract
+                {
+                    Provider = new Provider
+                    {
+                        Name = "Provider2"
+                    },
+                    Buyer=new Buyer
+                    {
+                        Name = "Buyer2",
+                        DateOfBirth = new DateTime(1990,2,2)
+                    }
+                }
+            });
+        }
     }
 }
