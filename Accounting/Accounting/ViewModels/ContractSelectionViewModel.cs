5a3d422f7f4421c1be4b42ece96198ef5087b073
mihaistancu <stancu_meehigh@yahoo.com>
2017-08-25 18:42:03 +0200

    Added viewmodel for selecting a contract

---
+++ Accounting/Accounting/ViewModels/ContractSelectionViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Accounting.Models;
+using System.Collections.ObjectModel;
+
+namespace Accounting.ViewModels
+{
+    class ContractSelectionViewModel
+    {
+        public ObservableCollection<Contract> Contracts { get; set; }
+
+        public Contract SelectedContract { get; set; }
+    }
+}
