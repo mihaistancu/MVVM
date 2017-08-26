e1d32396bd4a7b4e8e36f8b57fe5101db1cea06c
mihaistancu <stancu_meehigh@yahoo.com>
2017-08-26 23:55:15 +0300

    Extracted data access to a separate class

---
+++ Accounting/Accounting/Models/Services/ContractsService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Accounting.Models.Services
+{
+    public class ContractsService
+    {
+        private List<Provider> allProviders;
+        private List<Buyer> allBuyers;
+        private List<Contract> allContracts;
+
+        public ContractsService()
+        {
+            var provider1 = new Provider { Name = "Provider1" };
+            var provider2 = new Provider { Name = "Provider2" };
+
+            var buyer1 = new Buyer { Name = "Buyer1", DateOfBirth = new DateTime(1990, 1, 1) };
+            var buyer2 = new Buyer { Name = "Buyer2", DateOfBirth = new DateTime(1990, 2, 2) };
+
+            var contract1 = new Contract { Number = "1", Provider = provider1, Buyer = buyer1 };
+            var contract2 = new Contract { Number = "2", Provider = provider2, Buyer = buyer2 };
+            var contract3 = new Contract { Number = "4", Provider = provider1, Buyer = buyer2 };
+
+            allProviders = new List<Provider>(new[] { provider1, provider2 });
+            allBuyers = new List<Buyer>(new[] { buyer1, buyer2 });
+            allContracts = new List<Contract>(new[] { contract1, contract2, contract3 });
+        }
+
+        public List<Contract> GetContractsByProvider(Provider provider)
+        {
+            return allContracts.Where(c => c.Provider == provider).ToList();
+        }
+        
+        public List<Contract> GetContractsByBuyer(Buyer buyer)
+        {
+            return allContracts.Where(c => c.Buyer == buyer).ToList();
+        }
+
+        public List<Contract> GetContractsByProviderAndBuyer(Provider provider, Buyer buyer)
+        {
+            return allContracts.Where(c => c.Provider == provider && c.Buyer == buyer).ToList();
+        }
+
+        public List<Contract> GetAllContracts()
+        {
+            return allContracts;
+        }
+    }
+}
--- Accounting/Accounting/ViewModels/ContractSelectionViewModel.cs
+++ Accounting/Accounting/ViewModels/ContractSelectionViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Accounting.Models;
+using Accounting.Models.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,10 +8,8 @@ namespace Accounting.ViewModels
 {
     public class ContractSelectionViewModel : BindableBase
     {
-        private List<Provider> allProviders;
-        private List<Buyer> allBuyers;
-        private List<Contract> allContracts;
-
+        private ContractsService contractsService;
+        
         private List<Provider> filteredProviders;
 
         public List<Provider> FilteredProviders
@@ -62,8 +61,8 @@ namespace Accounting.ViewModels
         private void OnSelectedProviderChanged()
         {
             FilteredContracts = selectedBuyer == null
-                ? allContracts.Where(c => c.Provider == selectedProvider).ToList()
-                : allContracts.Where(c => c.Provider == selectedProvider && c.Buyer == selectedBuyer).ToList();
+                ? contractsService.GetContractsByProvider(selectedProvider)
+                : contractsService.GetContractsByProviderAndBuyer(selectedProvider, selectedBuyer);
 
             FilteredBuyers = FilteredContracts.Select(c => c.Buyer).Distinct().ToList();
         }
@@ -71,8 +70,8 @@ namespace Accounting.ViewModels
         private void OnSelectedBuyerChanged()
         {
             FilteredContracts = selectedProvider == null
-                ? allContracts.Where(c => c.Buyer == selectedBuyer).ToList()
-                : allContracts.Where(c => c.Provider == selectedProvider && c.Buyer == selectedBuyer).ToList();
+                ? contractsService.GetContractsByBuyer(selectedBuyer)
+                : contractsService.GetContractsByProviderAndBuyer(selectedProvider, selectedBuyer);
 
             FilteredProviders = FilteredContracts.Select(c => c.Provider).Distinct().ToList();
         }
@@ -89,23 +88,11 @@ namespace Accounting.ViewModels
 
         public ContractSelectionViewModel()
         {
-            var provider1 = new Provider { Name = "Provider1" };
-            var provider2 = new Provider { Name = "Provider2" };
+            contractsService = new ContractsService();
 
-            var buyer1 = new Buyer { Name = "Buyer1", DateOfBirth = new DateTime(1990, 1, 1) };
-            var buyer2 = new Buyer { Name = "Buyer2", DateOfBirth = new DateTime(1990, 2, 2) };
-
-            var contract1 = new Contract { Number = "1", Provider = provider1, Buyer = buyer1 };
-            var contract2 = new Contract { Number = "2", Provider = provider2, Buyer = buyer2 };
-            var contract3 = new Contract { Number = "4", Provider = provider1, Buyer = buyer2 };
-
-            allProviders = new List<Provider>(new[] { provider1, provider2 });
-            allBuyers = new List<Buyer>(new[] { buyer1, buyer2 });
-            allContracts = new List<Contract>(new[] { contract1, contract2,  contract3 });
-
-            filteredProviders = allProviders;
-            filteredBuyers = allBuyers;
-            filteredContracts = allContracts;
+            FilteredContracts = contractsService.GetAllContracts();
+            FilteredProviders = FilteredContracts.Select(c=>c.Provider).Distinct().ToList();
+            FilteredBuyers = FilteredContracts.Select(c => c.Buyer).Distinct().ToList();
         }
     }
 }
