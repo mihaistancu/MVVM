839a92733ea33ae8272480994e49432686a3f4e5
mihaistancu <stancu_meehigh@yahoo.com>
2017-08-29 17:58:51 +0300

    Use EntityFramework

---
+++ Accounting/Accounting/Models/DataAccess/ContractsDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System.Data.Entity;
+
+namespace Accounting.Models.DataAccess
+{
+    class ContractsDbContext : DbContext
+    {
+        public DbSet<Contract> Contracts { get; set; }
+        public DbSet<Provider> Providers { get; set; }
+        public DbSet<Buyer> Buyers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contract>().HasKey(c => c.Number);
+            modelBuilder.Entity<Provider>().HasKey(c => c.Name);
+            modelBuilder.Entity<Buyer>().HasKey(c => c.Name);
+        }
+    }
+}
--- Accounting/Accounting/Models/Services/ContractsService.cs
+++ Accounting/Accounting/Models/Services/ContractsService.cs
@@ -1,50 +1,69 @@
-﻿using System;
+﻿using Accounting.Models.DataAccess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data.Entity;
 
 namespace Accounting.Models.Services
 {
     public class ContractsService
     {
-        private List<Provider> allProviders;
-        private List<Buyer> allBuyers;
-        private List<Contract> allContracts;
-
         public ContractsService()
         {
-            var provider1 = new Provider { Name = "Provider1" };
-            var provider2 = new Provider { Name = "Provider2" };
+            using (var dbContext = new ContractsDbContext())
+            {
+                dbContext.Providers.RemoveRange(dbContext.Providers);
+                dbContext.Buyers.RemoveRange(dbContext.Buyers);
+                dbContext.Contracts.RemoveRange(dbContext.Contracts);
 
-            var buyer1 = new Buyer { Name = "Buyer1", DateOfBirth = new DateTime(1990, 1, 1) };
-            var buyer2 = new Buyer { Name = "Buyer2", DateOfBirth = new DateTime(1990, 2, 2) };
+                var provider1 = new Provider { Name = "Provider1" };
+                var provider2 = new Provider { Name = "Provider2" };
 
-            var contract1 = new Contract { Number = "1", Provider = provider1, Buyer = buyer1 };
-            var contract2 = new Contract { Number = "2", Provider = provider2, Buyer = buyer2 };
-            var contract3 = new Contract { Number = "4", Provider = provider1, Buyer = buyer2 };
+                var buyer1 = new Buyer { Name = "Buyer1", DateOfBirth = new DateTime(1990, 1, 1) };
+                var buyer2 = new Buyer { Name = "Buyer2", DateOfBirth = new DateTime(1990, 2, 2) };
 
-            allProviders = new List<Provider>(new[] { provider1, provider2 });
-            allBuyers = new List<Buyer>(new[] { buyer1, buyer2 });
-            allContracts = new List<Contract>(new[] { contract1, contract2, contract3 });
+                var contract1 = new Contract { Number = "1", Provider = provider1, Buyer = buyer1 };
+                var contract2 = new Contract { Number = "2", Provider = provider2, Buyer = buyer2 };
+                var contract3 = new Contract { Number = "4", Provider = provider1, Buyer = buyer2 };
+
+                dbContext.Contracts.AddRange(new[] { contract1, contract2, contract3 });
+                dbContext.SaveChanges();
+            }
         }
 
         public List<Contract> GetContractsBy(Provider provider)
         {
-            return allContracts.Where(c => c.Provider == provider).ToList();
+            using (var dbContext = new ContractsDbContext())
+            {
+                return dbContext.Contracts.Where(c => c.Provider == provider).ToList();
+            }   
         }
         
         public List<Contract> GetContractsBy(Buyer buyer)
         {
-            return allContracts.Where(c => c.Buyer == buyer).ToList();
+            using (var dbContext = new ContractsDbContext())
+            {
+                return dbContext.Contracts.Where(c => c.Buyer == buyer).ToList();
+            }   
         }
 
         public List<Contract> GetContractsBy(Provider provider, Buyer buyer)
         {
-            return allContracts.Where(c => c.Provider == provider && c.Buyer == buyer).ToList();
+            using (var dbContext = new ContractsDbContext())
+            {
+                return dbContext.Contracts.Where(c => c.Provider == provider && c.Buyer == buyer).ToList();
+            }
         }
 
         public List<Contract> GetAllContracts()
         {
-            return allContracts;
+            using (var dbContext = new ContractsDbContext())
+            {
+                return dbContext.Contracts
+                    .Include(c => c.Provider)
+                    .Include(c => c.Buyer)
+                    .ToList();
+            }
         }
     }
 }
